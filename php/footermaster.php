72cb11ae167a033069eeaeb60cd9af1f8feea992
shubhshubha <117938271+shubhshubha@users.noreply.github.com>
2022-11-10 23:48:04 +0530

    Add files via upload

---
+++ php/footermaster.php
@@ -0,0 +1,74 @@
+ 
+	<div class="row bg-darkred footer mt-3">
+	    <div class="col-sm-4 text-light text-center py-3">
+                <img src="images\aboutlogo.png" class="w-50 mt-5"/>
+                <p style="font-weight: bolder">
+                    Nalanda Open University is a state Open University<br>  duly established
+                    under the act of Bihar Government<br>  and is also recognized by Distance
+                    Education Council,<br>  IGNOU, Maidan Garhi, New Deihi.
+                    
+                </p>
+                <img src="images\facebook.png" style="height: 80px" class="fa-spin"/>
+               <img src="images\whatsapp.png" style="height: 70px" class="fa-spin"/>
+               <img src="images\instagram.png" style="height: 80px" class="fa-spin"/>
+               <img src="images\google.png" style="height: 70px" class="fa-spin"/>
+               <img src="images\twitter.png" style="height: 70px" class="fa-spin"/>
+              
+	    </div>
+             <div class="col-sm-2 text-light line" class="text-center">
+                 <h1 class="mt-5 text-center">Useful Links</h1>
+		 <div  class="loading">  <!--user defined class loading start-->
+                    <marquee direction="right" >
+                      <div>
+                      </div>
+                    </marquee>
+                 </div>                  <!--user defined class loading end-->
+                 <marquee direction="up" vspace="60px" style="height:190px;" onmouseover="stop()" onmouseleave="start()">
+                 <ul type="none" class="text-center">
+                     <li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-5 me-2"><a href="index.jsp" class="me-5 ms-3" >Home</a></i></li>
+                     <li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-3 me-2"><a href="aboutsite.jsp" class="me-5 ms-2" >About Site</a></i></li>
+                     <li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-3 me-2"><a href="services.jsp" class="me-5 ms-3" >Services</a></i></li>
+                     <li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-3 me-2"><a href="contact_us.jsp" class="me-5 ms-3" >Contact Us</a></i></li>
+                     <!--<li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-3 me-2"><a href="courses.jsp" class="me-5 ms-3" >Courses</a></i></li>-->
+                     <!--<li> <i class="fa-solid fa-magnifying-glass-arrow-right mt-3 me-2"><a href="study_center.jsp" class="me-5 ms-2" >Study Center</a></i></li>-->
+                 </ul>
+                 </marquee> 
+             </div>
+             <div class="col-sm-2 text-light text-center">
+		   <h1 class="mt-5 text-center">Contact Info</h1>
+		 <div  class="loading ">  <!--user defined class loading start-->
+                    <marquee direction="right" >
+                      <div>
+                      </div>
+                    </marquee>
+                 </div>                  <!--user defined class loading end-->
+                 <p class="mt-5" style="font-weight: bolder;">
+                     Government Polytechnic Jaunpur,Uttar Pradesh.
+                 </p>
+                 <p class="mt-5" >
+                     <i class="fa-solid fa-phone-volume mt-3"> <span style="font-family: cursive">+91 8303526676</span><br/><br/><span style="font-family:sans-serif">Give us a call</span></i>
+                 </p>
+	    </div>
+             <div class="col-sm-2 text-center text-light ">
+                 <h1 class="mt-5 text-center" >Subscribe</h1>
+                 <div  class="loading">  <!--user defined class loading start-->
+                    <marquee direction="right" >
+                      <div>
+                      </div>
+                    </marquee>
+                 </div>                  <!--user defined class loading end-->
+                 <img src="images\aboutlogo.png" class="w-50 mt-4 " style="height:150px; "/>
+                <p class="mt-1" style="font-weight: bolder">
+                    Fill the details below to receive the manual of the GP Jaunpur.
+                </p>
+	    </div>
+            <div class="col-sm-2 text-center text-light">
+                
+            </div>
+	</div>
+        <div class="row text-center text-light">
+            <div class="col-sm-12 bg-maroon">
+                <p><span class="text-primary">&copy </span>CopyRight 2022 Desined & Developed By | <span class="text-primary">Shubham Maurya </span>|<span class="text-info"> Government Polytechnic Jaunpur</span></p>
+            </div>
+            <script src="js\main.js"></script>
+        </div>
